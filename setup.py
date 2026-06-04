b7988bd8100efec610bf11ccbe3cdfeeb20c3fa1
lazox12 <tomik.lazar@gmail.com>
2026-06-04 15:39:34 +0200

    fixed v0.1.5

--- setup.py
+++ setup.py
@@ -10,19 +10,10 @@ mylib_module = Extension(
     libraries=['pthread', 'dl'],
     language='c++'
 )
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
 
 setup(
-    name='simpleTFTPS',
-    description='a simple TFTPS client',
     ext_modules=[mylib_module],
-    packages=[],
     # Ensure headers and other files are included in the sdist
     package_data={'': ['include/*.hpp', 'simpleTFTPS/target/release/libsimpleTFTPS.a']},
     include_package_data=True,
-    use_scm_version=True,
-    setup_requires=['setuptools_scm'],
-    long_description=long_description,
-    long_description_content_type="text/markdown",
 )
